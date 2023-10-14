2a90decac52df0af87560890bf6f5c189c9c66d0
Fjodor Pasitsnik <fjodor.pasitsnik@tktk.ee>
2023-10-14 11:37:38 -0200

    add week6 code

---
+++ NumberArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[5];
+Random rnd = new Random();
+
+int sum = 0;
+
+//populating in the array
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(1, numbers.Length + 25);
+    sum = sum + numbers[i];
+}
+
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine($"total:{sum}");
